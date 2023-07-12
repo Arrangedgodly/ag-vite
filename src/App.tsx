50964eb61cb216e72f2e89d2b96c34309ddadbfb
Grady Wasil <90996993+Arrangedgodly@users.noreply.github.com>
2023-07-11 23:25:51 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-5xl font-bold">Welcome to the Vite Starter Kit!</h1>
+      <p className="text-xl mt-5">This kit includes React, Vite, Typescript, TailwindCSS, daisyUI, Firebase, and some other goodies.</p>
+    </div>
+  )
+}
+
+export default App
