e6c9f395115de4234557a5851fb0e5c74fbbe89c
Grady <arrangedgodly@gmail.com>
2023-07-12 23:54:46 -0600

    refactor: first page edits. adding album covers and starting to screw with react parallax

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-function App() {
+import ParallaxContainer from "./components/ParallaxContainer";
+import TTNE from "./assets/ttne.png";
+import kotw from "./assets/kotw.png";
+import godlyshistory from "./assets/godlyshistory.png";
+import repressingrage from "./assets/repressingrage.png";
+import run from "./assets/run.png";
+import sa from "./assets/sa.png";
+import stack from "./assets/stack.png";
+import faraway from "./assets/faraway.png";
+import ruminations from "./assets/ruminations.png";
 
+function App() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-5xl font-bold">Welcome to the Vite Starter Kit!</h1>
-      <p className="text-xl mt-5">This kit includes React, Vite, Typescript, TailwindCSS, daisyUI, Firebase, and some other goodies.</p>
+    <div className="flex flex-col items-center min-h-screen w-screen">
+      <ParallaxContainer
+        image={TTNE}
+        name="Taxed Tolled and Eternally Trolled"
+      />
+      <ParallaxContainer image={run} name="run.exe" />
+      <ParallaxContainer image={faraway} name="Rhyme or a Reason" />
+      <ParallaxContainer image={godlyshistory} name="Godly's History" />
+      <ParallaxContainer image={kotw} name="King of the Wicked Remix" />
+      <ParallaxContainer image={repressingrage} name="Repressing Rage" />
+      <ParallaxContainer image={stack} name="$Stack" />
+      <ParallaxContainer image={ruminations} name="Ruminations" />
+      <ParallaxContainer image={sa} name="Seasonal Aggression: The Mixtape" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ParallaxContainer.tsx
@@ -0,0 +1,24 @@
+import { Parallax } from "react-parallax";
+
+interface ParallaxContainerProps {
+  image: any;
+  name: string;
+}
+
+const ParallaxContainer = ({ image, name }: ParallaxContainerProps) => {
+  return (
+    <Parallax
+      blur={{ min: -15, max: 15 }}
+      bgImage={image}
+      bgImageAlt={name}
+      strength={500}
+      bgClassName="bg-cover bg-center bg-repeat bg-fixed"
+      className="my-10 w-screen flex flex-col items-center justify-center text-2xl font-bold text-white h-[250px]"
+    >
+      {name}
+      <div style={{ height: "50px" }} />
+    </Parallax>
+  );
+};
+
+export default ParallaxContainer;
