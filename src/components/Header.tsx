841d297cda32bca99684f181b6f15b5cdbdbdd17
Grady <arrangedgodly@gmail.com>
2023-07-19 17:17:40 -0600

    refactor: adding more images and starting to change the layout a bit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
+import Logo from '../assets/name-logo.svg';
+
 const Header = () => {
   return (
     <div className="navbar fixed top-0 left-0 bg-base-300 z-50">
       <div className='navbar-start'></div>
       <div className='navbar-center'>
-        <h1 className='text-3xl m-5'>Arranged Godly</h1>
+        <img src={Logo} alt="Logo" className="w-250 h-20" />
       </div>
       <div className='navbar-end'></div>
     </div>
